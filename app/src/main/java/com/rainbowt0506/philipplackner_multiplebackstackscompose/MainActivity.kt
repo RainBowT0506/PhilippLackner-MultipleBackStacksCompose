b4e040393b3e71d47c3ad5d99b4c4d133696ce5c
RainBowT0506 <rainbowt0506@gmail.com>
2025-05-31 17:04:53 +0800

    initial commit

---
+++ app/src/main/java/com/rainbowt0506/philipplackner_multiplebackstackscompose/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.rainbowt0506.philipplackner_multiplebackstackscompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rainbowt0506.philipplackner_multiplebackstackscompose.ui.theme.PhilippLacknerMultipleBackStacksComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PhilippLacknerMultipleBackStacksComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PhilippLacknerMultipleBackStacksComposeTheme {
+        Greeting("Android")
+    }
+}
