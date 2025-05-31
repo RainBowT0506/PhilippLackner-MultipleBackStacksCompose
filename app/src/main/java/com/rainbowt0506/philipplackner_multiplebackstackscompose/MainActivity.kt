e565f7986f88f662944149ae89d8fb84f02c50c2
RainBowT0506 <rainbowt0506@gmail.com>
2025-05-31 17:25:11 +0800

    feat: Implement bottom navigation bar with icons and route switching

--- app/src/main/java/com/rainbowt0506/philipplackner_multiplebackstackscompose/MainActivity.kt
+++ app/src/main/java/com/rainbowt0506/philipplackner_multiplebackstackscompose/MainActivity.kt
@@ -5,43 +5,87 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.rainbowt0506.philipplackner_multiplebackstackscompose.ui.theme.PhilippLacknerMultipleBackStacksComposeTheme
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.rainbowt0506.philipplackner_multiplebackstackscompose.model.BottomNavigationItem
+import com.rainbowt0506.philipplackner_multiplebackstackscompose.ui.theme.MultipleBackStacksComposeTheme
+
+val items = listOf(
+    BottomNavigationItem(
+        title = "Home",
+        route = "home",
+        selectedIcon = Icons.Filled.Home,
+        unselectedIcon = Icons.Outlined.Home
+    ),
+    BottomNavigationItem(
+        title = "Chat",
+        route = "chat",
+        selectedIcon = Icons.Filled.Email,
+        unselectedIcon = Icons.Outlined.Email
+    ),
+    BottomNavigationItem(
+        title = "Settings",
+        route = "settings",
+        selectedIcon = Icons.Filled.Settings,
+        unselectedIcon = Icons.Outlined.Settings
+    )
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PhilippLacknerMultipleBackStacksComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val navController = rememberNavController()
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentRoute = navBackStackEntry?.destination?.route
+            MultipleBackStacksComposeTheme {
+                Scaffold(
+                    bottomBar = {
+                        NavigationBar {
+                            items.forEach { item ->
+                                val isSelected = item.route == currentRoute
+                                NavigationBarItem(
+                                    selected = isSelected,
+                                    label = { Text(item.title) },
+                                    icon = {
+                                        Icon(
+                                            imageVector = if (isSelected) item.selectedIcon else item.unselectedIcon,
+                                            contentDescription = item.title
+                                        )
+                                    },
+                                    onClick = {
+                                        navController.navigate(item.route) {
+                                            popUpTo(navController.graph.findStartDestination().id) {
+                                                saveState = false
+                                            }
+                                            launchSingleTop = true
+                                            restoreState = false
+                                        }
+                                    }
+                                )
+                            }
+                        }
+                    },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PhilippLacknerMultipleBackStacksComposeTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/rainbowt0506/philipplackner_multiplebackstackscompose/ui/theme/Theme.kt
+++ app/src/main/java/com/rainbowt0506/philipplackner_multiplebackstackscompose/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.rainbowt0506.philipplackner_multiplebackstackscompose.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -34,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun PhilippLacknerMultipleBackStacksComposeTheme(
+fun MultipleBackStacksComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
